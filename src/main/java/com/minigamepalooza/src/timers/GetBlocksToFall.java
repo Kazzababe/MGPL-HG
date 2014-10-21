5e551e257da19d54a4bdc31bfc75ce2e041f111c
Paul Sauve <paul@paulbgd.me>
2014-10-21 18:20:56 -0500

    Fix block dropping

--- src/main/java/com/minigamepalooza/src/timers/GetBlocksToFall.java
+++ src/main/java/com/minigamepalooza/src/timers/GetBlocksToFall.java
@@ -1,64 +1,59 @@
 package com.minigamepalooza.src.timers;
 
+import com.minigamepalooza.src.HungerGames;
 import java.util.Collections;
 import java.util.Comparator;
-
 import net.minecraft.server.v1_7_R4.World;
-
 import org.bukkit.Location;
 import org.bukkit.block.Block;
 import org.bukkit.craftbukkit.v1_7_R4.CraftWorld;
 import org.bukkit.craftbukkit.v1_7_R4.util.CraftMagicNumbers;
 import org.bukkit.scheduler.BukkitRunnable;
 
-import com.minigamepalooza.src.HungerGames;
-
 public class GetBlocksToFall extends BukkitRunnable {
-	
-	@SuppressWarnings("deprecation")
-	@Override
-	public void run() {
-		Location c1 = HungerGames.WORLD_SPAWN.getDimensions()[0].getLocation(HungerGames.WORLD), 
-				 c2 = HungerGames.WORLD_SPAWN.getDimensions()[1].getLocation(HungerGames.WORLD);
-		World world = ((CraftWorld) c1.getWorld()).getHandle();
-        for(int x = c1.getBlockX(); x < c2.getBlockX(); x++) {
-            int y = c1.getBlockY();
-            for(int z = c1.getBlockZ(); z < c2.getBlockZ(); z++) {
-                int type = CraftMagicNumbers.getId(world.getType(x, y, z));
-                for(int i = 0; i < 256; i++) {
-                    type = CraftMagicNumbers.getId(world.getType(x, i, z));
-                    if(type != 0) {
+
+    @SuppressWarnings("deprecation")
+    @Override
+    public void run() {
+        Location c1 = HungerGames.WORLD_SPAWN.getDimensions()[0].getLocation(HungerGames.WORLD),
+                c2 = HungerGames.WORLD_SPAWN.getDimensions()[1].getLocation(HungerGames.WORLD);
+        World world = ((CraftWorld) c1.getWorld()).getHandle();
+        for (int x = c1.getBlockX(); x < c2.getBlockX(); x++) {
+            for (int z = c1.getBlockZ(); z < c2.getBlockZ(); z++) {
+                for (int i = 0; i < 256; i++) {
+                    int type = CraftMagicNumbers.getId(world.getType(x, i, z));
+                    if (type != 0) {
                         HungerGames.BLOCKS.add(c1.getWorld().getBlockAt(x, i, z));
                     }
                 }
             }
         }
-        
+
         final Location spawn = HungerGames.WORLD_SPAWN.getFeastLocation().getLocation(HungerGames.WORLD);
         Collections.sort(HungerGames.BLOCKS, new Comparator<Block>() {
-			@Override
-			public int compare(Block b1, Block b2) {
-				int d1 = this.getDistance(b1), d2 = this.getDistance(b2);
-				if(d1 > d2) {
-					return -1;
-				} else if(d2 > d1) {
-					return 1;
-				} else {
-					if(b1.getY() > b2.getY()) {
-						return 1;
-					} else if(b2.getY() > b1.getY()) {
-						return -1;
-					}
-				}
-				return 0;
-			}
-			
-			private int getDistance(Block b1) {
-				Location l = b1.getLocation();
-				return (int) Math.sqrt((l.getX() - spawn.getX()) * (l.getX() - spawn.getX()) + (l.getZ() - spawn.getZ()) * (l.getZ() - spawn.getZ()));
-			}
+            @Override
+            public int compare(Block b1, Block b2) {
+                double d1 = this.getDistance(b1), d2 = this.getDistance(b2);
+                if (d1 > d2) {
+                    return -1;
+                } else if (d2 > d1) {
+                    return 1;
+                } else {
+                    if (b1.getY() > b2.getY()) {
+                        return 1;
+                    } else if (b2.getY() > b1.getY()) {
+                        return -1;
+                    }
+                }
+                return 0;
+            }
+
+            private double getDistance(Block b1) {
+                Location l = b1.getLocation();
+                return ((l.getX() - spawn.getX()) * (l.getX() - spawn.getX())) + ((l.getZ() - spawn.getZ()) * (l.getZ() - spawn.getZ()));
+            }
         });
         HungerGames.GAME_READY = true;
         System.out.println("[Hunger Games] The game is ready to play!");
-	}
+    }
 }
--- src/main/java/com/minigamepalooza/src/timers/MakeAllDaBootysDrop.java
+++ src/main/java/com/minigamepalooza/src/timers/MakeAllDaBootysDrop.java
@@ -1,60 +1,64 @@
 package com.minigamepalooza.src.timers;
 
+import com.minigamepalooza.src.HungerGames;
 import java.util.ArrayList;
-import java.util.HashSet;
 import java.util.List;
-import java.util.Set;
-
 import org.bukkit.Location;
 import org.bukkit.Material;
 import org.bukkit.block.Block;
 import org.bukkit.scheduler.BukkitRunnable;
 
-import com.minigamepalooza.src.HungerGames;
-
 public class MakeAllDaBootysDrop extends BukkitRunnable {
-	private int radius = Integer.MAX_VALUE;
-	private List<Block> blocks = new ArrayList<Block>();
-	
-	private Location spawn;
-	
-	public MakeAllDaBootysDrop() {
-		this.spawn = HungerGames.WORLD_SPAWN.getFeastLocation().getLocation(HungerGames.WORLD);
-		this.radius = getDistance(HungerGames.BLOCKS.get(0));
-		this.blocks = new ArrayList<Block>(HungerGames.BLOCKS);
-	}
-	
-	@Override
-	public void run() {
-		int maxIterations = 100;
-		int iterations = 0;
-		
-		Set<Block> toRemove = new HashSet<Block>();
-		for(int i = 0; i < this.blocks.size(); i++) {
-			if(iterations >= maxIterations) {
-				break;
-			}
-			int distance = getDistance(this.blocks.get(i));
-			if(distance >= this.radius) {
-				this.makeDaBootyDrop(this.blocks.get(i));
-				toRemove.add(this.blocks.get(i));
-			} else {
-				this.radius -= 2;
-				break;
-			}
-			iterations++;
-		}
-		this.blocks.removeAll(toRemove);
-	}
-	
-	@SuppressWarnings("deprecation")
-	private void makeDaBootyDrop(Block block) {
-		//block.getWorld().spawnFallingBlock(block.getLocation(), block.getType(), block.getData());
-		block.setType(Material.AIR);
-	}
-	
-	private int getDistance(Block b1) {
-		Location l = b1.getLocation();
-		return (int) Math.sqrt((l.getX() - this.spawn.getX()) * (l.getX() - this.spawn.getX()) + (l.getZ() - this.spawn.getZ()) * (l.getZ() - this.spawn.getZ()));
-	}
+    //private int radius = Integer.MAX_VALUE;
+    private List<Block> blocks = new ArrayList<Block>();
+
+    private Location spawn;
+
+    public MakeAllDaBootysDrop() {
+        this.spawn = HungerGames.WORLD_SPAWN.getFeastLocation().getLocation(HungerGames.WORLD);
+        //this.radius = getDistance(HungerGames.BLOCKS.get(0));
+        this.blocks = new ArrayList<Block>(HungerGames.BLOCKS);
+    }
+
+    @Override
+    public void run() {
+        int maxIterations = 100;
+        int iterations = 0;
+
+        while (!this.blocks.isEmpty() && iterations++ < maxIterations) {
+            makeDaBootyDrop(this.blocks.remove(0));
+        }
+
+        if (this.blocks.isEmpty()) {
+            cancel(); // all done
+        }
+        /* Removed, because I have no idea what's up with this
+        Set<Block> toRemove = new HashSet<Block>();
+        for (Block block : this.blocks) {
+            if (iterations >= maxIterations) {
+                break;
+            }
+            int distance = getDistance(block);
+            if (distance >= this.radius) {
+                this.makeDaBootyDrop(block);
+                toRemove.add(block);
+            } else {
+                this.radius -= 2;
+                break;
+            }
+            iterations++;
+        }
+        this.blocks.removeAll(toRemove);*/
+    }
+
+    @SuppressWarnings("deprecation")
+    private void makeDaBootyDrop(Block block) {
+        //block.getWorld().spawnFallingBlock(block.getLocation(), block.getType(), block.getData());
+        block.setType(Material.AIR);
+    }
+
+    private int getDistance(Block b1) {
+        Location l = b1.getLocation();
+        return (int) Math.sqrt((l.getX() - this.spawn.getX()) * (l.getX() - this.spawn.getX()) + (l.getZ() - this.spawn.getZ()) * (l.getZ() - this.spawn.getZ()));
+    }
 }
